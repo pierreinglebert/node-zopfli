91958ba3b76c75b346a491af8993c608328b8460
Pierre Inglebert <pierre.inglebert@gmail.com>
2015-05-01 10:28:52 +0200

    add jshintrc

--- test/zopfliStream.js
+++ test/zopfliStream.js
@@ -1,41 +1,40 @@
-var chai = require("chai");
-var fs = require("fs");
+'use strict';
+
+var chai = require('chai');
+var fs = require('fs');
 var zlib = require('zlib');
 var async = require('async');
+var path = require('path');
 var zopfli = require('../lib/zopfli');
+var stream = require('stream');
+var util = require('util');
 
 var assert = chai.assert;
 
-var path = require("path");
+var fixturesPath = path.join(__dirname, 'fixtures');
 
-//just a tool function to rm dir recursively
-var rmdir = function(dir) {
-  var list = fs.readdirSync(dir);
-  for(var i = 0; i < list.length; i++) {
-    var filename = path.join(dir, list[i]);
-    var stat = fs.statSync(filename);
-    if(filename == "." || filename == "..") {
-    } else if(stat.isDirectory()) {
-      rmdir(filename);
-    } else {
-      fs.unlinkSync(filename);
-    }
-  }
-  fs.rmdirSync(dir);
+function MemoryStream () {
+  stream.Writable.call(this);
+  this.data = new Buffer(0);
+}
+util.inherits(MemoryStream, stream.Writable);
+MemoryStream.prototype._write = function (chunk, encoding, done) {
+  this.data = Buffer.concat([this.data, chunk]);
+  done();
 };
 
 var testStream = function(deflate, inflate, done) {
   var files = fs.readdirSync('test/fixtures');
   async.eachSeries(files, function(file, next) {
-    var writeStream = fs.createWriteStream('tmp/'  + file);
-    writeStream.on('error', function(err) {
-      next(err);
-    });
-    writeStream.on('close', function() {
-      assert.equal(fs.readFileSync('test/fixtures/' + file).toString(), fs.readFileSync('tmp/'  + file).toString());
+    var writeStream = new MemoryStream();
+    writeStream.on('finish', function() {
+      assert.equal(
+        fs.readFileSync(path.join(fixturesPath, file)).toString(),
+        writeStream.data.toString()
+      );
       next();
     });
-    fs.createReadStream('test/fixtures/' + file)
+    fs.createReadStream(path.join(fixturesPath, file))
       .pipe(deflate())
       .pipe(inflate())
       .pipe(writeStream);
@@ -48,28 +47,25 @@ var testStream = function(deflate, inflate, done) {
 
 describe('Zopfli stream',function() {
   before(function(done) {
-    if(fs.existsSync('tmp')) rmdir('tmp');
-    fs.mkdirSync('tmp');
-    randData = require('randomstring').generate(1 * 1024 * 1024);
-    fs.writeFileSync('test/fixtures/big');
-    done();
+    fs.writeFile(path.join(fixturesPath, 'big'), require('randomstring').generate(1 * 1024 * 1024), done);
   });
   after(function(done) {
-    rmdir('tmp');
-    fs.unlinkSync('test/fixtures/big');
-    done();
+    fs.unlink(path.join(fixturesPath, 'big'), done);
   });
   describe('deflate',function() {
+    this.timeout(20000); // let time to deflate a big file
     it('should deflate using stream by node zlib', function(done){
       testStream(zopfli.createDeflate, zlib.createInflateRaw, done);
     });
   });
   describe('zlib',function() {
+    this.timeout(20000); // let time to deflate a big file
     it('should inflate using stream by node zlib', function(done){
       testStream(zopfli.createZlib, zlib.createInflate, done);
     });
   });
   describe('gzip',function() {
+    this.timeout(20000); // let time to deflate a big file
     it('should inflate using stream by node gzip', function(done){
       testStream(zopfli.createGzip, zlib.createGunzip, done);
     });
