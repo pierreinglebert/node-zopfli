b5a1e980727feb8ade729dafb704bdbc18123172
XhmikosR <xhmikosr@gmail.com>
2015-05-02 08:13:47 +0300

    Assorted lint fixes.

--- test/zopfliBufferSync.js
+++ test/zopfliBufferSync.js
@@ -1,43 +1,48 @@
+/* jshint mocha: true */
+
 'use strict';
 
 var chai = require('chai');
 var fs = require('fs');
+var path = require('path');
 var zlib = require('zlib');
 var async = require('async');
 var zopfli = require('../lib/zopfli');
 
 var assert = chai.assert;
 
+var fixturesPath = path.join(__dirname, 'fixtures');
+
 var testBufferSync = function(deflate, inflate, done) {
   var files = fs.readdirSync('test/fixtures');
   async.eachSeries(files, function(file, next) {
-    var buffer = fs.readFileSync('test/fixtures/' + file);
+    var buffer = fs.readFileSync(path.join(fixturesPath, file));
     var result = deflate(buffer, {});
     inflate(result, function(err, result) {
-      if(!err) {
-        assert.equal(result.toString(), fs.readFileSync('test/fixtures/' + file).toString());
+      if (!err) {
+        assert.equal(result.toString(), fs.readFileSync(path.join(fixturesPath, file)).toString());
       }
       next(err);
     });
   },
-  function(err){
+  function(err) {
     done(err);
   });
 };
 
-describe('Zopfli buffer sync',function() {
-  describe('deflate',function() {
-    it('should deflate synchronously using buffer by node zlib', function(done){
+describe('Zopfli buffer sync', function() {
+  describe('deflate', function() {
+    it('should deflate synchronously using buffer by node zlib', function(done) {
       testBufferSync(zopfli.deflateSync, zlib.inflateRaw, done);
     });
   });
-  describe('zlib',function() {
-    it('should zlib synchronously using buffer by node zlib', function(done){
+  describe('zlib', function() {
+    it('should zlib synchronously using buffer by node zlib', function(done) {
       testBufferSync(zopfli.zlibSync, zlib.inflate, done);
     });
   });
-  describe('gzip',function() {
-    it('should deflate synchronously using buffer by node zlib', function(done){
+  describe('gzip', function() {
+    it('should deflate synchronously using buffer by node zlib', function(done) {
       testBufferSync(zopfli.gzipSync, zlib.gunzip, done);
     });
   });
